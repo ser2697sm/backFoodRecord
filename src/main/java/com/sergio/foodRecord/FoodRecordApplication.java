ef9440b7730746a902f9256cde2a42e4bb7334b5
ser2697sm <sergiomartinez26m@gmail.com>
2025-01-17 17:03:29 +0100

    commit base

---
+++ src/main/java/com/sergio/foodRecord/FoodRecordApplication.java
@@ -0,0 +1,13 @@
+package com.sergio.foodRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergio/foodRecord/FoodRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.foodRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
